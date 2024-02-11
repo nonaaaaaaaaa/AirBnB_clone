37f7b6eeeb89a86a8195eb957236d36632046ab4
Deadeen <141264828+Deadeen@users.noreply.github.com>
2024-02-11 21:00:09 +0200

    as test py

--- tests/test_console.py
+++ tests/test_console.py
@@ -631,7 +631,7 @@ class TestHBNBCommand_count(unittest.TestCase):
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("MyModel.count()"))
             self.assertEqual("** class doesn't exist **",
-                            output.getvalue().strip())
+            output.getvalue().strip())
 
     def test_count_object(self):
         with patch("sys.stdout", new=StringIO()) as output:
